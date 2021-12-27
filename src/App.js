8f41b50d0d4f32d71f61175f04faadc4a9f506e6
OrangeHoopla <quadekirby@gmail.com>
2021-12-27 12:34:30 -0500

    please worj

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
   	
 	    <div className="App">
-	    	<h2>help me</h2>
+	    	<h2>help god</h2>
 		    <Routes>
 		    	<Route path="/" element={<Login />} />
 		    </Routes>
